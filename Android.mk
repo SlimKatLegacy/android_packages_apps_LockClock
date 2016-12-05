8c948714e657736ee608ddbec0b7f60239338f94
spezi77 <spezi77@gmx.com>
2016-12-05 17:23:59 +0100

    Debloat the app by omitting hi-res resource files. * HDPI is sufficient for devices with small displays (e.h. htc bravo has 480x800).
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- Android.mk
+++ Android.mk
@@ -22,7 +22,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_SDK_VERSION := 19
 LOCAL_PACKAGE_NAME := LockClock
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
-LOCAL_AAPT_INCLUDE_ALL_RESOURCES := true
+PRODUCT_AAPT_PREF_CONFIG := hdpi
 
 # Include res dir from chips
 google_play_dir := ../../../external/google/google_play_services/libproject/google-play-services_lib/res
